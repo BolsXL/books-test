4c24b30f7aef226b841e840ef4eecb4aa8e9081e
bols <joelmer13@gmail.com>
2021-06-03 22:51:59 -0400

    Ready for more

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
+// VARIABLES
 let myLibrary = [];
+const books = document.querySelector('.books');
 
+// RUNNING CODE
+
+// FUNCTIONS
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read == true ? "read" : "not read yet";
+    this.read = read == true ? "Read" : "Not read yet";
     this.info = function() {
         return `${this.title} by ${this.author}, ${this.pages}, ${this.read}.`
     }
 }
 
 function addBookToLibrary(book) {
-
+    myLibrary.push(book);
+    printLibrary();
 }
+
+function printLibrary() {
+    books.innerHTML = "";
+    myLibrary.forEach(book => {
+        let newListItem = document.createElement('li');
+        let text = `${book.title} by ${book.author}. (${book.pages} pages) [${book.read}]`
+        newListItem.textContent = text;
+        books.appendChild(newListItem);
+    });
+}
+
+// TESTING STUFF
