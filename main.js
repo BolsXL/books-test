684f36c65b7ed2c7b9556088488b0603a06be147
bols <joelmer13@gmail.com>
2021-06-05 09:55:50 -0400

    Finished but dirty

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
+'use strict'
 // VARIABLES
-let myLibrary = [];
+let book1 = new Book("Moby Dick", "Herman Melville", 540, false);
+let book2 = new Book("Title", "Author", 500, false);
+let myLibrary = [book1, book2];
 const books = document.querySelector('.books');
+const newBook = document.querySelector('.new-book');
 
 // RUNNING CODE
+window.onload = printLibrary();
+newBook.addEventListener('click', e => {
+    let title = prompt("Add a title.");
+    let author = prompt("Add an author.");
+    let pages = prompt("How many pages?");
+    let read = false;
+    let book = new Book(title, author, pages, read);
+    addBookToLibrary(book);
+})
 
 // FUNCTIONS
 function Book(title, author, pages, read) {
@@ -18,6 +31,69 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(book) {
     myLibrary.push(book);
     printLibrary();
+}
+
+function removeBookFromLibrary(book) {
+    let arr = myLibrary.filter(item => item.title !== book.title);
+    myLibrary = arr;
+    printLibrary();
+}
+
+function toggleRead(book) {
+    if (book.read == "Read") book.read = "Not read yet";
+    else if (book.read == "Not read yet") book.read = "Read";
+    printLibrary();
+}
+
+function printLibrary() {
+    books.innerHTML = '';
+    myLibrary.forEach(book => {
+        let li = document.createElement('li');
+        li.textContent = `${book.title} by ${book.author}, ${book.pages} pages, ${book.read}`;
+
+        let newDiv = document.createElement('div');
+        let delBtn = document.createElement('button');
+
+        delBtn.classList.add("delete");
+        delBtn.textContent = 'delete';
+        delBtn.addEventListener('click', e => {
+            let title = e.target.parentNode.parentNode.textContent.split(' by ')[0];
+
+            myLibrary.forEach(book => {
+                if (title == book.title) {
+                    removeBookFromLibrary(book);
+                }
+            });
+
+            printLibrary();
+        });
+
+        let readBtn = document.createElement('button');
+        readBtn.classList.add("read");
+        readBtn.textContent = 'toggle read';
+        readBtn.addEventListener('click', e => {
+            let title = e.target.parentNode.parentNode.textContent.split(' by ')[0];
+
+            myLibrary.forEach(book => {
+                if (title == book.title) {
+                    if (book.read === "Read") {
+                        book.read = "Not read yet";
+                    }
+                    else if (book.read === "Not read yet") {
+                        book.read = "Read";
+                    }
+                }
+            });
+
+            printLibrary();
+        })
+
+        newDiv.appendChild(delBtn);
+        newDiv.appendChild(readBtn);
+
+        li.appendChild(newDiv);
+        books.appendChild(li);
+    });
 }
 
 // TESTING STUFF
